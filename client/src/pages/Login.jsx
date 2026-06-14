a42498133700ec6593d32c16cef4721ee3294c93
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-06-14 14:56:46 +0530

    refactor: update Login component to use email and password fields with state management

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,19 +3,32 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
+    const [loginData, setLoginData] = useState({
+        email: "",
+        password: "",
+    });
 
-    const navigate = useNavigate();
+    const [validateError, setValidateError] = useState();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
 
-        console.log("Username : ", userName)
-        console.log("password : ", password)
-    }
+        setLoginData((prevData) => ({ ...prevData, [name]: value }));
+    };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Handle login logic here, e.g., send loginData to the server
+        //Validate loginData
 
+        console.log("Login data submitted:", loginData);
+
+        const payload = {
+            email: loginData.email.toLowerCase(),
+            password: loginData.password,
+        };
+    };
     return (
         <>
             <div className='h-[90vh] bg-[url("/foodTable.webp")] bg-yellow-400 grid items-center justify-start bg-cover bg-center md:ps-30 '>
@@ -25,22 +38,36 @@ const Login = () => {
                         <p className='text-center'>Login to your Cravings account</p>
                     </div>
                     <form onSubmit={handleSubmit}>
-                        
+
                         <div className='grid mb-4'>
-                            <label htmlFor="username" className='mb-2 text-1xl'>Username</label>
-                            <input type="text" name='username' id='username' placeholder='Enter your username' value={userName} onChange={(e) => setUserName(e.target.value)} className='border border-[var(--bg-color)] focus:outline focus:outline-[var(--bg-color)] p-1 rounded focus:outline-2' />
+                            <label htmlFor="email" className='mb-2 text-1xl'>Email</label>
+                            <input
+                                type="email"
+                                name='email'
+                                id='email'
+                                placeholder='Enter your email'
+                                value={loginData.email}
+                                onChange={handleChange}
+                                className='border border-[var(--bg-color)] focus:outline focus:outline-[var(--bg-color)] p-1 rounded focus:outline-2' />
                         </div>
                         <div className='grid mb-4'>
                             <label htmlFor="password" className='mb-2 text-1xl'>Password</label>
-                            <input type="text" name='password' id='password' placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)} className='border border-[var(--bg-color)] focus:outline focus:outline-[var(--bg-color)] p-1 rounded focus:outline-2' />
+                            <input
+                                type="password"
+                                name='password'
+                                id='password'
+                                placeholder='Enter your password'
+                                value={loginData.password}
+                                onChange={handleChange}
+                                className='border border-[var(--bg-color)] focus:outline focus:outline-[var(--bg-color)] p-1 rounded focus:outline-2' />
                         </div>
                         <button type='submit' className='w-full py-2 mb-4 rounded-md bg-[var(--bg-color)] text-white text-lg'>Login</button>
                         <div className='flex items-center justify-center gap-2 mb-2'>
                             <div className='w-17 border' />
                             <p className=''>Don't have an account?</p>
-                            <div className='w-17 border'/>
+                            <div className='w-17 border' />
                         </div>
-                        <p className='text-center '><span  onClick={()=> navigate("/register")} className='text-[var(--bg-color)] cursor-pointer '>Create an account</span></p>
+                        <p className='text-center '><span onClick={() => navigate("/register")} className='text-[var(--bg-color)] cursor-pointer '>Create an account</span></p>
                     </form>
                 </div>
             </div>
