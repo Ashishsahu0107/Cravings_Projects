16f6bcbf254430e05fb63518c393fea340466f1f
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-06-21 13:12:38 +0530

    feat: add Card component with sample data for user profiles

---
+++ client/src/components/ui/Card.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Card = () => {
+
+    const card = [
+        {
+            name: "Ashish",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, obcaecati ab. Quis veniam odit vitae aperiam sit minima quae, enim qui, veritatis magni ipsam soluta assumenda eaque iste cum similique.",
+            roll: "Student"
+        },
+        {
+            name: "shubham",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, obcaecati ab. Quis veniam odit vitae aperiam sit minima quae, enim qui, veritatis magni ipsam soluta assumenda eaque iste cum similique.",
+            roll: "Student"
+        },
+        {
+            name: "prasoon",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, obcaecati ab. Quis veniam odit vitae aperiam sit minima quae, enim qui, veritatis magni ipsam soluta assumenda eaque iste cum similique.",
+            roll: "Student"
+        }
+    ]
+
+
+    // const course = {
+    //     title: name.trim(),
+    //     description: "Learn React from basics",
+    //     instructor: "Ashish Sahu",
+    //     duration: "20 Hours"
+    // };
+
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export default Card
