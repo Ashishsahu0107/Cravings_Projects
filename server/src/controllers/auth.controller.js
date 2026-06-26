3b843a8d75e23be7b47a07f8bf5ec610ed459e7a
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-06-26 20:37:20 +0530

    feat: add user authentication and database connection
    - Implemented MongoDB connection in dbConnection.config.js
    - Created user model with schema validation in user.model.js
    - Developed user registration and login functionality in auth.controller.js
    - Added error handling for required fields and existing users during registration
    - Included response messages for successful user creation and login

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,83 @@
+import User from "../models/user.model.js";
+
+export const RegisterUser = async (req, res, next) => {
+
+    try {
+        const { fullName, email, password, phone, gender, dob } = req.body;
+
+        if (!fullName || !email || !password || !phone || !gender || !dob) {
+            const error = new Error("All fields Required");
+            error.statusCode = 400;
+            return next(error);
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            const error = new Error("Email already registred");
+            error.statusCode = 409;
+            return next(error);
+        }
+
+        const photoUrl = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
+
+        const photo = {
+            url: photoUrl,
+            publicId: null,
+        };
+
+        const newUser = await User.create({
+            fullName,
+            email,
+            password,
+            phone,
+            gender,
+            dob,
+            photo,
+        });
+
+        res.status(201).json({ message: "User Created Successfully" });
+
+    } catch (error) {
+        next();
+    }
+};
+
+export const LoginUser = async (req, res,next) => {
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            const error = new Error("All fields Required");
+            error.statusCode = 400;
+            return next(error);
+        }
+
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser) {
+            const error = new Error("Email not registred");
+            error.statusCode = 404;
+            return next(error);
+        }
+
+        if (password !== existingUser.password) {
+            const error = new Error("Incorrect Password");
+            error.statusCode = 401;
+            return next(error);
+        }
+
+        res.status(200).json({
+            message: "WelCome Back",
+            data: existingUser,
+        });
+
+    } catch (error) {
+        console.log(error.message);
+        next();
+    }
+};
+
+export const LogoutUser = (req, res) => {
+    
+};
---
+++ server/src/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const UserSchema = mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        phone: {
+            type: String,
+        },
+        gender: {
+            type: String,
+            required: true,
+        },
+        photo: {
+            url: {
+                type: String,
+            },
+            publicId: {
+                type: String,
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        dob: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model("User",UserSchema);
+
+export default User;
