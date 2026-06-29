5c7637015ff33fbb1989ec6e73c8767759654dac
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-06-29 22:33:02 +0530

    feat: add axios configuration and integrate morgan and cors middleware in server

--- server/index.js
+++ server/index.js
@@ -5,10 +5,15 @@ import express from 'express';
 import AuthRouter from './src/routers/auth.route.js';
 import PublicRouter from './src/routers/public.route.js';
 import connectDB from './src/config/dbConnection.config.js';
+import morgan from 'morgan'
+import cors from 'cors'
 
-const app = express(); // object
+const app = express();
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
+
+app.use(morgan("dev"));
 
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
